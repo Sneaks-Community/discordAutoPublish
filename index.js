cd8bd2ec898c9f0a760883a44e0224d02d68c3b8
Frumpy <chris.johnson9638@gmail.com>
2020-11-11 02:59:27 -0500

    trying setTimeout loop

--- index.js
+++ index.js
@@ -10,47 +10,50 @@ let queue = {}
 bot.on("ready", () => {
     console.log("Signed in as " + bot.user.tag);
 
-    for(let id of config.channels){
+    for (let id of config.channels) {
         queue[id] = [];
     }
-    
+
     // console.log(queue)
 
-    
+
 
     // bot.setInterval(run, 3660000)
     bot.setInterval(run, 45000)
 })
 
 bot.on("message", message => {
-    if(config.channels.includes(message.channel.id)){
+    if (config.channels.includes(message.channel.id)) {
         console.log("message in " + message.channel.name)
         queue[message.channel.id].push(message);
         console.log(queue[message.channel.id].length)
     }
 })
 
-async function run(){
+function run() {
     //let pushArr = queue.slice(0, 10);
 
     console.log("start")
     console.time("start")
 
-    for(let channel in queue){
+    for (let channel in queue) {
         let pushArr = queue[channel].slice(0, 10);
         queue[channel] = queue[channel].slice(10);
         console.log("whats left: " + queue[channel])
 
         //console.table(pushArr)
 
+        // let i = 0;
 
         for(let i = 0; i < pushArr.length; i++) {
-            setTimeout(async () => {
-              pushArr[i].crosspost().catch(err => {console.log(err)});
-            }, 1500);
-          }
+        // while (i < pushArr.length) {
+            setTimeout(() => {
+                pushArr[i].crosspost().catch(err => { console.log(err) });
+            }, 5000);
+            // i++;
+        }
 
-        
+
     }
     console.timeEnd("start")
     //console.log(Object.keys(queue).length)
